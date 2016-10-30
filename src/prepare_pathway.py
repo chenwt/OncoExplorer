df06619626bea23932942c54ce1029615bf083fd
Yifeng Tao <taoyf12@sina.com>
2016-10-29 21:21:00 -0400

    + save2txt_list

--- src/prepare_pathway.py
+++ src/prepare_pathway.py
@@ -58,33 +58,16 @@ def save2txt(path, table):
         print >> f, '\t'.join(row)
     f.close()
 
-# def get_sga2deg(path_sga2deg_all, path_sga2deg_train, patid_train):
-#     print 'reading from: {}...'.format(path_sga2deg_all)
-#     sga2deg = dd(float)
-#     sga2deg_str = []
-#     sga2deg_all_list = set()
-#     sga_corpus = set()
-#     deg_corpus = set()
-#     for line in open(path_sga2deg_all, 'r'):
-#         values = line.strip().split('\t')
-#         patid,sga,deg,prob = int(values[0]),values[1],values[2],float(values[3])
-#         sga2deg_all_list.add((sga,deg))
-#         if patid not in patid_train: continue
-#         sga2deg[(sga,deg)]+= prob
-#         sga_corpus.add(sga)
-#         deg_corpus.add(deg)
-
-#     for row in sga2deg.keys():
-#         sga2deg_str.append('sga2deg'+'\t'+row[0]+'\t'+row[1]+'\t'+str(sga2deg[row]))
-
-#     print 'saving to {}...'.format(path_sga2deg_train)
-#     f = open(path_sga2deg_train,'w')
-#     for row in sga2deg_str:
-#         print >> f, row
-#     f.close
-
-#     sga2deg = sga2deg.keys()
-#     return sga2deg,sga_corpus,deg_corpus,sga2deg_all_list
+def save2txt_list(path, table):
+    '''
+    path: the filename to be saved.
+    table: list/set of string, data to be saved.
+    '''
+    print 'saving to {}...'.format(path)
+    f = open(path, 'w')
+    for row in table:
+        print >> f, row
+    f.close()
 
 def writeSample(path, filename, sga2deglist, deg_corpus):
     with io.open(path+'/tmp','w') as file:
@@ -212,7 +195,6 @@ def step2(path_outputData):
 
 
     # get the training(?) edgesself.
-
     NUM_PAT = 4468
     patid_list = range(1,NUM_PAT+1)
     SEED = 888
@@ -378,8 +360,8 @@ if __name__ == '__main__':
     path_inputData = root+'/inputData'
     path_outputData = root+'/outputData'
 
-    # step1(path_inputData,path_outputData)
-    # step2(path_outputData)
+    #step1(path_inputData,path_outputData)
+    #step2(path_outputData)
     step3(path_outputData)
 
 
