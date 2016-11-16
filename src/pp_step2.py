21b719d303f57d565ff3858a7f95c5a54e72ed6b
yifengtao <taoyf12@sina.com>
2016-11-16 14:34:01 -0500

    step2

---
+++ src/pp_step2.py
@@ -0,0 +1,33 @@
+from collections import defaultdict as dd
+import io
+import os
+import random
+import argparse
+
+if __name__ == '__main__':
+    ''' Second step to generate the graph, training and test data for ProPPR.
+    Usage:
+    python pp_step2.py --inputData <dir contatins ensemble.txt> --outputData <dir to contain output> 
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inputData', help = 'directory of input data', type = str)
+    parser.add_argument('--outputData', help = 'directory of output data', type = str)
+    parser.add_argument('--noisyor', help = 'use noisy-or or simply sum up',type = bool)
+    parser.add_argument('--threshold', help = 'threshold of whether a (sga,deg) pair exists', type = float)
+    args = parser.parse_args()
+
+    path_inputData = args.inputData 
+    path_outputData = args.outputData
+    noisyor = args.noisyor
+    threshold = args.threshold
+
+
+    # shuffle training and test
+
+    print 'reading from... {}'.format(path_inputData+'/ensemble.txt')
+    for line in open(path_inputData+'/ensemble.txt', 'r'):
+        line = line.split('\t')
+        pat, patid, can, canid, sga, deg, prob = line
+        print pat, patid, can, canid, sga, deg, prob
+
+#End
