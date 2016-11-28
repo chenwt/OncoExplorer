dea8385e5174ea656b6e4d4681e83c80eb6c4df1
yifengtao <taoyf12@sina.com>
2016-11-28 17:57:54 -0500

    all raw data

--- src/extract_link.py
+++ src/extract_link.py
@@ -51,18 +51,37 @@ if __name__ == '__main__':
     relation = 'linked'
     pos_start = len(relation) + 1
 
+
+    pi3k = set()
+
+    f = open('/home/yifengt/Github/inputData/pi3k.txt','r')
+    for line in f:
+        line = line.strip().lower() 
+        pi3k.add(line)
+    f.close()
+    pi3k_sel = dd(int) 
+
     link = extract(path_inputData+'/test_linked.solutions.txt', pos_start)
     print len(link)
     link_set = set()
     for line in link.keys():
+        if link[line] == 1: continue
         src, dst = line[0],line[1]
         if (dst,src) in link_set: continue
         link_set.add((src,dst))
+        pi3k_sel[src], pi3k_sel[dst] = 0, 0
+        if src in pi3k: pi3k_sel[src] = 1
+        if dst in pi3k: pi3k_sel[dst] = 1
     print len(link_set)
     f = open(path_outputData+'/linked.txt', 'w')
-    print >> f, 'src\tdst'
+    print >> f, 'Source\tTarget\tType'
     for line in link_set:
-        print >> f, line[0]+'\t'+line[1]
+        print >> f, line[0]+'\t'+line[1]+'\tUndirected'
+    f.close()
+    f = open(path_outputData+'/node.txt', 'w')
+    print >> f, 'Id\tPi3k'
+    for line in pi3k_sel.keys():
+        print >> f, line + '\t'+ str(pi3k_sel[line])
     f.close()
 
     print 'Done!'
