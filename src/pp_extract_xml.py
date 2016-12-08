6cbc70437ceb44d473f387c9ed648dcdeab2bdb6
yifengtao <taoyf12@sina.com>
2016-12-08 03:10:21 -0500

    debug

--- src/pp_extract_xml.py
+++ src/pp_extract_xml.py
@@ -4,6 +4,33 @@ import os
 import random
 import argparse
 
+def extract_xml(name_xml, name_pathway, path_inputData, path_outputData):
+    print 'reading from... {}'.format(path_inputData+'/'+name_xml+'.xml')
+    k = 0
+    entity_type = None
+    gene_name = None
+    f = open(path_inputData+'/'+name_xml+'.xml', 'r')
+    for _ in range(7): next(f)
+    for line in f:
+        k += 1
+        if k % 5 == 1:
+            line = line.strip().split(' ')[-1]
+            entity_type = line.split('\"')[1]
+            if entity_type == 'group': break
+        if k % 5 == 3:
+            if entity_type != 'gene': continue
+            line = line.strip().split('\"')[1]
+            line = line.split(',')[0]
+            gene_name = line.split('.')[0]
+            gene_name = gene_name.lower()
+            set_gene.add(gene_name)
+
+    f = open(path_outputData+'/'+name_pathway+'.txt', 'w')
+    for line in set_gene:
+        print >> f, line
+    f.close()
+    return
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--inputData', help = 'directory of input data', type = str)
@@ -14,28 +41,14 @@ if __name__ == '__main__':
     path_inputData = args.inputData 
     path_outputData = args.outputData
     pathwayName = args.pathwayName
+    pathwayName = pathwayName.split(',')
     set_gene = set()
 
-    print 'reading from... {}'.format(path_inputData+'/'+pathwayName+'.xml')
-    k = 0
-    entity_type = None
-    gene_name = None
-    f = open(path_inputData+'/'+pathwayName+'.xml', 'r')
-    for line in f:
-        k += 1
-        if k % 5 == 1:
-            line = line.strip().split(' ')[-1]
-            entity_type = line.split('\"')[1]
-        if k % 5 == 3:
-            if entity_type != 'gene': continue
-            line = line.strip().split('\"')[1]
-            line = line.split(',')[0]
-            gene_name = line.split('.')[0]
-            gene_name = gene_name.lower()
-            set_gene.add(gene_name)
+    pathway2xml = {'pi3k':'hsa04151','cancer':'hsa05200'}
+    for name_pathway in pathwayName:
+        name_xml = pathway2xml[name_pathway]
+        extract_xml(name_xml, name_pathway, path_inputData, path_outputData)
 
-    f = open(path_outputData+'/'+pathwayName+'.txt', 'w')
-    for line in set_gene:
-        print >> f, line
-    f.close()
+
+
 #EOF.
