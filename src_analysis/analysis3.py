2bcc30fb40aaf18246d24f343c2fd1e79ba40c0b
Yifeng Tao <taoyf12@sina.com>
2017-01-07 07:17:50 -0500

    Add files via upload

--- src_analysis/analysis3.py
+++ src_analysis/analysis3.py
@@ -13,12 +13,15 @@ if __name__ == '__main__':
     path_outputData = args.outputData
 
     f = open(path_inputData+'/go-basic.obo', 'r')
-    fo = open(path_outputData+'/process.txt', 'w')
+    # Format: isA GoTermX GoTermY
+    # Meaning: GoTermX is GoTermY
+    fo = open(path_outputData+'/isA.cfacts', 'w')
+
     flag_term = False
     #flag_biopro = true
     src_id = ''
     name_space = ''
-    dst_id = ''
+    dst_id = list()
     for line in f:
         line = line.strip()
         if line == '[Term]': flag_term = True
@@ -33,17 +36,30 @@ if __name__ == '__main__':
             name_space = line
             continue
         if line.startswith('is_a: GO:'):
-            if dst_id != '': dst_id += '\n' 
-            dst_id += line
+            line = line.split(':')[2].split(' ! ')[0]
+            dst_id.append('go'+line)
+            # if dst_id != '': dst_id += '\n' 
+            # dst_id += line
             continue
         if line == '':
             
             if name_space == 'namespace: biological_process':
             #print >> fo, '======================='
-                print >> fo, src_id
-                print >> fo, name_space
-                print >> fo, dst_id
-            dst_id = ''
+                src = 'go'+src_id.split(':')[2]
+
+                for dst in dst_id:
+                    print >> fo, 'isA\t'+src+'\t'+dst
+                # print >> fo, src
+                # #print >> fo, name_space
+                # print >> fo, ' '.join(dst_id)
+                #for dst in dst_id:
+                #    print >> fo, dst
+                # if dst_id == '':
+                #     dst_id = 'terminal'
+                # else:
+                #     dst_id = dst_id.split(':')[2]
+                # print >> fo, dst_id
+            dst_id = list()
             flag_term = False
             continue
         #print line
