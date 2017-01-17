e3e1fbdc402c8395a6f1df752ead66fb75cd7fac
Yifeng Tao <taoyf12@sina.com>
2017-01-16 21:03:59 -0500

    Spectral clustering for the SGAs.

---
+++ SpectralClustering/main.m
@@ -0,0 +1,7 @@
+% main.m
+% Realize the spectral clusering of Andrew Ng, Michael Jordan and Yair
+% Weiss.
+% Functions modified from:
+% https://www.mathworks.com/matlabcentral/fileexchange/34412-fast-and-efficient-spectral-clustering
+
+
