9401619b63f3e45fda02e9838ee3fa0d42439d8e
Yifeng Tao <taoyf12@sina.com>
2017-01-17 22:57:04 -0500

    +Checking patients.

--- src_analysis/analysis05.py
+++ src_analysis/analysis05.py
@@ -22,12 +22,17 @@ if __name__ == '__main__':
     set_old = set()
     set_new = set()
 
+    pat_mine = set()
+    pat_old = set()
+    pat_new = set()
     with open(path_inputData+'/ensemble.txt') as f:
         next(f)
         for line in f:
             line = line.strip().split('\t')
             pat, patid, can, sga, deg, prob = line[0], int(line[1]),line[2],line[4],line[5],line[6]
             set_mine.add((pat,sga,deg))
+            pat_mine.add(pat)
+    f.close()
 
     with open(path_inputData+'/YX_TDI_SGA_DEG_Tumor.csv') as f:
         next(f)
@@ -35,6 +40,8 @@ if __name__ == '__main__':
             line = line.strip().split(',')
             pat, sga, deg = line[0].lower(), line[1].lower(), line[2].lower()
             set_old.add((pat,sga,deg))
+            pat_old.add(pat)
+    f.close()
 
     with open(path_inputData+'/YX_TDIresult_PanCancerAtlas_CT01_114.csv') as f:
         next(f)
@@ -42,7 +49,13 @@ if __name__ == '__main__':
             line = line.strip().split(',')
             pat, sga, deg = line[0].lower(), line[1].lower(), line[2].lower()
             set_new.add((pat,sga,deg))
+            pat_new.add(pat)
+    f.close()
 
     print '#mine={}\t#old={}\t#new={}'.format(len(set_mine), len(set_old), len(set_new))
+    print '# old intersection mine ={}'.format(len(set_mine.intersection(set_old)))
+    print '# old intersection new ={}'.format(len(set_old.intersection(set_new)))
 
+    print '#mine={}\t#old={}\t#new={}'.format(len(pat_mine), len(pat_old), len(pat_new))
+    print '# old intersection new ={}'.format(len(pat_old.intersection(pat_new)))
 #EOF.
