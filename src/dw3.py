bf25cc77c4e9e11d876bbf83dfbc1e166f74067a
Yifeng Tao <taoyf12@sina.com>
2017-02-19 10:15:28 -0500

    Deep walk on sga-sga graph.

---
+++ src/dw3.py
@@ -0,0 +1,15 @@
+#dw3
+from multiprocessing import Pool
+import subprocess
+import glob,os
+import random
+#deepwalk --output result2/brca_km40_ss_wl1_nw10.embeddings --number-walks 10 --walk-length 1 --workers 64 --representation-size 40 --input data/graph_brca.mat --format mat --matfile-variable-name network
+
+# function for multi-thread processing.
+def f(wl, nw):
+    subprocess.call("deepwalk --output result2/brca_km40_ss_wl"+str(wl)+"_nw"+str(nw)+".embeddings --number-walks "+str(nw)+" --walk-length "+str(wl)+" --workers 64 --representation-size 40 --input data/graph_brca.mat --format mat --matfile-variable-name network", shell=True)
+
+for wl in [1,2,3,5,10,20]:
+	for nw in [10,20,50,100,200]:
+		f(wl, nw)
+# EOF.
